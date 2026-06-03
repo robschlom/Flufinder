371864c0f6584bcd33b8da1d7b3a2ca0ee4cd08b
rosasmith12 <ros010@ucsd.edu>
2026-06-03 10:57:25 -0700

    first function 2

--- flufinder.R
+++ flufinder.R
@@ -21,3 +21,13 @@ splitpeptides_to_masses <- function(aa) {
   # Unlisting the inner lists to generate a list of vectors of masses for each protein
   lapply(peptide_masses, unlist)
 }
+
+trypsinize <- function(proteins) {
+  
+  #Opening stringr for simple string manipulation
+  library(stringr)
+  
+  #Using str_split_1 to split proteins after R or K amino acids; "(?<=R|K)"is a regular expression for splitting after "?<=" R or K "R|K"
+  lapply(proteins, str_split_1, pattern="(?<=R|K)")
+}
+
